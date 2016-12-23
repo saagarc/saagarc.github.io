da2db4a1693dd040a29c831e699ab3287944a6b1
Saagar <saagarchangoer@gmail.com>
2016-12-23 18:17:13 +0100

    1

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,33 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-  // Get started!
+// 	$(".pagebody").css("display", "none");
+//     $(".pagebody").fadeIn(2000);
 
+//     $("a.transition").click(function(event){
+//         event.preventDefault();
+//         linkLocation = this.href;
+//         $("pagebody").fadeOut(1000, redirectPage);      
+//     });
+         
+//     function redirectPage() {
+//         window.location = linkLocation;
+//     }
+// });
+
+
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.htm"
+    })
+    .when("/test", {
+        templateUrl : "test.htm"
+    })
+    .when("/green", {
+        templateUrl : "green.htm"
+    })
+    .when("/blue", {
+        templateUrl : "blue.htm"
+    });
 });
